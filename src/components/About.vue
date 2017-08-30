4582fb626c806f2aab7ee80effa0a0a994b9278a
DeveloperWakeling <developerwakeling@gmail.com>
2017-08-29 21:59:09 -0400

    Initial Setup of Vue Application with basic routes

---
+++ src/components/About.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="about">
+        <h1>Basic About Page</h1>
+        <p>{{message}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'about',
+  data () {
+    return {
+      message: 'My name is Michael Wakeling and I am a software engineer'
+    }
+  }
+}
+</script>
+<style scoped>
+#about {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+    text-align: center;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,17 +1,35 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import Vuetify from 'vuetify'
 import App from './App'
-import router from './router'
+import VueRouter from 'vue-router'
 
-Vue.use(Vuetify)
+
+// Importing the different components
+import Hello from './components/Hello'
+import About from './components/About'
+
+Vue.use(VueRouter)
+
+const routes = [
+  // Defining the routes for the application
+  {path: '/', component: Hello},
+  {path: '/about', component: About},
+]
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
 new Vue({
   el: '#app',
   router,
   template: '<App/>',
-  components: { App }
-})
+  components: {
+    App
+  }
+}).$mount('#app')
