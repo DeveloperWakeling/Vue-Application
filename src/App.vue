72844e55ef352f2983e037c342c8d194c400f862
DeveloperWakeling <developerwakeling@gmail.com>
2017-08-31 00:47:00 -0400

    Side Navigation WIP

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,42 @@
 <template>
   <div id="app">
     <md-toolbar>
-      <router-link class="md-button md-raised md-accent" v-bind:to="'/'">Home</router-link>
-      <router-link class="md-button md-raised" v-bind:to="'/about'">About</router-link>
+      <md-button class="md-icon-button" @click="toggleSideNav">
+        <md-icon>menu</md-icon>
+      </md-button>
+      <h2 class="md-title">DVLPR Wakeling</h2>
     </md-toolbar>
+    <md-sidenav class="md-left" ref="sidenav">
+      <md-toolbar>
+        <div class="md-toolbar-container">
+          <!-- <router-link class="md-button md-raised" v-bind:to="'/about'">About</router-link> -->
+          <h2>Navigation</h2>
+        </div>
+      </md-toolbar>
+      <md-list>
+        <md-list-item>
+          <router-link to='/'>Home</router-link>
+        </md-list-item>
+        <md-list-item>
+          <router-link to='/about'>About</router-link>
+        </md-list-item>
+      </md-list>
+    </md-sidenav>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  methods: {
+    toggleSideNav() {
+      this.$refs.sidenav.toggle();
+    },
+    test() {
+      debugger;
+    }
+  }
 }
 </script>
 
